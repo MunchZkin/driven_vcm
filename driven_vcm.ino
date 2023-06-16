99a38ec0f77ecd432b6c8e7b2a16f1f784f0d87e
WARMIND\Omkar <bno0506@gmail.com>
2023-06-17 00:48:48 +0100

    refactored to [1Hhz,100Hz,1Hz], performance metrics tasktime, overrun/s

--- driven_vcm.ino
+++ driven_vcm.ino
@@ -1,54 +1,118 @@
-const int ledPin =  LED_BUILTIN;
+unsigned long previousMillis1 = 0;  // Previous time for task1
+unsigned long previousMillis2 = 0;  // Previous time for task2
+unsigned long previousMillis3 = 0;  // Previous time for task3
 
-// Variables will change:
-int ledState = LOW;
+const unsigned long interval1 = 1;  // Task1 interval in milliseconds (1 kHz)
+const unsigned long interval2 = 10; // Task2 interval in milliseconds (100 Hz)
+const unsigned long interval3 = 1000; // Task3 interval in milliseconds (1 Hz)
 
-unsigned long previousMillis_1kHz = 0;
-unsigned long previousMillis_200Hz = 0;
-unsigned long previousMillis_50Hz = 0;
-unsigned long previousMillis_1Hz = 0;
+unsigned long taskTime1 = 0;  // Execution time for task1
+unsigned long taskTime2 = 0;  // Execution time for task2
+unsigned long taskTime3 = 0;  // Execution time for task3
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+unsigned long overrun1 = 0;  // Number of task1 overruns
+unsigned long overrun2 = 0;  // Number of task2 overruns
+unsigned long overrun3 = 0;  // Number of task3 overruns
+
+unsigned long overrunsPerSecond = 0;  // Total overruns per second
+unsigned long previousSecond = 0;  // Previous second count
+
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+int ledState = LOW; 
+
+void task1()
+{
+//  1khz task
 }
 
-void loop() {
+void task2()
+{
+//  100hz task
+}
+
+void task3()
+{
+  statusLED();
+}
+
+void statusLED()
+{
+  if (ledState == LOW) {
+    ledState = HIGH;
+  }
+  else {
+    ledState = LOW;
+  }
+  digitalWrite(ledPin, ledState);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+}
+
+void loop()
+{
   unsigned long currentMillis = millis();
 
-  // 1ms (1khz) task time
-  if (currentMillis - previousMillis_1kHz >= 1) {
-    previousMillis_1kHz = currentMillis;
-    
-    // enter code here
-  }
+  // Task1 (1 kHz)
+  if (currentMillis - previousMillis1 >= interval1)
+  {
+    previousMillis1 = currentMillis;
+    unsigned long startTask1 = millis();
+    task1();
+    taskTime1 = millis() - startTask1;
 
-  // 5ms (200Hz) task time
-  if (currentMillis - previousMillis_200Hz >= 5) {
-    previousMillis_200Hz = currentMillis;
-    
-    // enter code here
-  }
-    
-  // 20ms (50Hz) task timee
-  if (currentMillis - previousMillis_50Hz >= 20) {
-    previousMillis_50Hz = currentMillis;
-    
-    // enter code here
-  }
-
-  // 1000ms (1Hz) task timee
-  if (currentMillis - previousMillis_1Hz >= 1000) {
-    previousMillis_1Hz = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
+    if (taskTime1 > interval1)
+    {
+      overrun1++;
     }
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
   }
-  
+
+  // Task2 (100 Hz)
+  if (currentMillis - previousMillis2 >= interval2)
+  {
+    previousMillis2 = currentMillis;
+    unsigned long startTask2 = millis();
+    task2();
+    taskTime2 = millis() - startTask2;
+
+    if (taskTime2 > interval2)
+    {
+      overrun2++;
+    }
+  }
+
+  // Task3 (1 Hz)
+  if (currentMillis - previousMillis3 >= interval3)
+  {
+    previousMillis3 = currentMillis;
+    unsigned long startTask3 = millis();
+    task3();
+    taskTime3 = millis() - startTask3;
+
+    if (taskTime3 > interval3)
+    {
+      overrun3++;
+    }
+  }
+
+  // Calculate overruns per second
+  unsigned long currentSecond = millis() / 1000;
+  if (currentSecond != previousSecond)
+  {
+    overrunsPerSecond = overrun1 + overrun2 + overrun3;
+    overrun1 = 0;
+    overrun2 = 0;
+    overrun3 = 0;
+    previousSecond = currentSecond;
+    Serial.print(taskTime1);
+    Serial.print(", ");
+    Serial.print(taskTime2);
+    Serial.print(", ");
+    Serial.println(taskTime3);
+    
+  }
 }
