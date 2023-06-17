ba95b5af32767c2b5f657c31d601208e758a01dc
WARMIND\Omkar <bno0506@gmail.com>
2023-06-17 15:11:59 +0100

    added temperature, voltage, current measurement

--- driven_vcm.ino
+++ driven_vcm.ino
@@ -1,44 +1,66 @@
-unsigned long previousMillis1 = 0;  // Previous time for task1
-unsigned long previousMillis2 = 0;  // Previous time for task2
-unsigned long previousMillis3 = 0;  // Previous time for task3
+const int throttle_Pin = A0;               // Analog throttle input pin
+const int brake_Pin = 2;                   // Digital brake input pin
+const int reverse_Pin = 3;                 // Digital reverse input pin
+const int temperatureSensor1_Pin = A1;     // Analog input pin for temperature sensor 1
+const int temperatureSensor2_Pin = A2;     // Analog input pin for temperature sensor 2
+const int voltageSensor1_Pin = A3;         // Analog input pin for voltage sensor 1
+const int voltageSensor2_Pin = A4;         // Analog input pin for voltage sensor 2
+const int currentSensor_Pin = A5;          // Analog input pin for current sensor
+const int motorSpeedSensor_Pin = 6;        // Digital input pin for motor speed sensor
+const int wheelSpeedSensor_Pin = 7;        // Digital input pin for wheel speed sensor
 
-const unsigned long interval1 = 1;  // Task1 interval in milliseconds (1 kHz)
-const unsigned long interval2 = 10; // Task2 interval in milliseconds (100 Hz)
-const unsigned long interval3 = 1000; // Task3 interval in milliseconds (1 Hz)
+unsigned long previousMillis1 = 0;         // Previous time for task1
+unsigned long previousMillis2 = 0;         // Previous time for task2
+unsigned long previousMillis3 = 0;         // Previous time for task3
 
-unsigned long taskTime1 = 0;  // Execution time for task1
-unsigned long taskTime2 = 0;  // Execution time for task2
-unsigned long taskTime3 = 0;  // Execution time for task3
+const unsigned long interval1 = 10;        // Task1 interval in milliseconds (100 Hz)
+const unsigned long interval2 = 10;        // Task2 interval in milliseconds (100 Hz)
+const unsigned long interval3 = 10;        // Task3 interval in milliseconds (100 Hz)
 
-unsigned long overrun1 = 0;  // Number of task1 overruns
-unsigned long overrun2 = 0;  // Number of task2 overruns
-unsigned long overrun3 = 0;  // Number of task3 overruns
+unsigned long taskTime1 = 0;               // Execution time for task1
+unsigned long taskTime2 = 0;               // Execution time for task2
+unsigned long taskTime3 = 0;               // Execution time for task3
 
-unsigned long overrunsPerSecond = 0;  // Total overruns per second
-unsigned long previousSecond = 0;  // Previous second count
+unsigned long overrun1 = 0;                // Number of task1 overruns
+unsigned long overrun2 = 0;                // Number of task2 overruns
+unsigned long overrun3 = 0;                // Number of task3 overruns
+
+unsigned long overrunsPerSecond = 0;       // Total overruns per second
+unsigned long previousSecond = 0;          // Previous second count
 
 // Led Status
-const int ledPin =  LED_BUILTIN;
+const int ledPin = LED_BUILTIN;
 int ledState = LOW;
 
-const int throttle_Pin = A0;  // Analog throttle input pin
-const int brake_Pin = 2;  // Analog throttle input pin
-const int reverse_Pin = 3;  // Analog throttle input pin
-const float filterFactor = 0.2;  // Filter factor (0.0 - 1.0)
+float filterFactor = 0.2;                  // Filter factor (0.0 - 1.0)
 
-bool brake_signalState = false;   // State of brake signal
-bool reverse_latchState = false;     // State of the reverse signal
+bool brake_signalState = false;            // State of brake signal
+bool reverse_latchState = false;           // State of the reverse signal
+
 
 void task1()
 {
-  int v_throttle = analogRead(throttle_Pin); // Read throttle input
+  int v_throttle = analogRead(throttle_Pin);      // Read throttle input
   static float v_throttle_filtered = v_throttle;  // Initialize the filtered value
   v_throttle_filtered = lowPassFilter(v_throttle, v_throttle_filtered);
 }
 
 void task2()
 {
-//  100hz task
+  // Read temperature sensor measurements
+  int temperature1 = analogRead(temperatureSensor1_Pin);
+  int temperature2 = analogRead(temperatureSensor2_Pin);
+
+  // Read voltage sensor measurements
+  int voltage1 = analogRead(voltageSensor1_Pin);
+  int voltage2 = analogRead(voltageSensor2_Pin);
+
+  // Read current sensor measurement
+  int current = analogRead(currentSensor_Pin);
+
+  // Read wheel speed sensor measurements
+  int motorSpeed = analogRead(motorSpeedSensor_Pin);
+  int wheelSpeed = analogRead(wheelSpeedSensor_Pin);
 }
 
 void task3()
@@ -66,6 +88,10 @@ float lowPassFilter(float input, float outputPrev)
 void setup()
 {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   
 }
